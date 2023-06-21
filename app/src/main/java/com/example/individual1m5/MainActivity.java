f13a3a65f14e0f628f436221ff13fed37dc6500e
Roberto Medina P <robertomedinamv@gmail.com>
2023-06-20 21:25:29 -0400

    boton salida funciona

--- app/src/main/java/com/example/individual1m5/MainActivity.java
+++ app/src/main/java/com/example/individual1m5/MainActivity.java
@@ -34,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finishAffinity();
+            }
+        });
 
 
     }
